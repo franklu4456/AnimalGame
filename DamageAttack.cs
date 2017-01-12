85dfd394ef18ffb0a366ecb32dbcb5772362b5f6
lugiamaster <lugiamaster88@gmail.com>
2017-01-12 11:37:17 -0500

    Add files via upload

---
+++ DamageAttack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGame
+{
+    class DamageAttack : Attack
+    {
+        private int _damage;
+
+        public DamageAttack(Type attackType, AttackEffect effect, int damage)
+            : base(attackType, effect)
+        {
+            AttackType = attackType;
+            Effect = effect;
+        }
+
+        public int Damage
+        {
+            get
+            {
+                return _damage;
+            }
+        }
+    }
+}
---
+++ StatChangeAttack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGame
+{
+    class StatChangeAttack : Attack
+    {
+        private Stat _statChangeValue;
+
+        public StatChangeAttack(Type attackType, AttackEffect effect, Stat statChangeValue)
+            : base(attackType, effect)
+        {
+            AttackType = attackType;
+            Effect = effect;
+        }
+
+        public Stat StatChangeValue
+        {
+            get
+            {
+                return _statChangeValue;
+            }
+        }
+    }
+}
