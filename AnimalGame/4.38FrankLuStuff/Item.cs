b33f06c6d134d5225d23029adac8214b18438cb7
Frank <FrankLucow@hotmail.com>
2017-01-22 16:40:08 -0500

    Add files via upload

---
+++ AnimalGame/4.38FrankLuStuff/Item.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGame
+{
+    class Item
+    {
+        private string _name;
+        private int _statNumber;
+        private int _quantity;
+        private int _price;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public int StatNumber
+        {
+            get
+            {
+                return _statNumber;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+            set
+            {
+                _quantity = value;
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+        }
+
+        public Item(string name, int statNumber, int quantity, int price)
+        {
+            _name = name;
+            _statNumber = statNumber;
+            _quantity = quantity;
+            _price = price;
+        }
+
+        public void UsedAtkBuff(Animal currentAnimal, Item atkBuff)
+        {
+            currentAnimal.Attack = currentAnimal.Attack + atkBuff.StatNumber;
+        }
+
+        public void UsedDefBuff(Animal currentAnimal, Item defBuff)
+        {
+            currentAnimal.Defense = currentAnimal.Defense + defBuff.StatNumber;
+        }
+
+        public void UsedSpeedBuff(Animal currentAnimal, Item speedBuff)
+        {
+            currentAnimal.Speed = currentAnimal.Speed + speedBuff.StatNumber;
+        }
+
+        public Animal UsedNet(Animal enemy, bool inBattle)
+        {
+            if (inBattle == false)
+            {
+                Random numberGenerator = new Random();
+                int randomChance = numberGenerator.Next(1, 11);
+
+                if (enemy.Health > (enemy.MaxHealth / 2))
+                {
+                    if (randomChance <= 3)
+                    {
+                        return enemy;
+                    }
+                }
+                else if (enemy.Health < (enemy.MaxHealth / 2) && enemy.Health >= (enemy.MaxHealth / 5))
+                {
+                    if (randomChance <= 5)
+                    {
+                        return enemy;
+                    }
+                }
+                else if (enemy.Health < (enemy.MaxHealth / 5))
+                {
+                    if (randomChance <= 7)
+                    {
+                        return enemy;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ AnimalGame/4.38FrankLuStuff/Store.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGame
+{
+    class Store
+    {
+        public void SetUpShop()
+        {
+            List<Item> availableItems = new List<Item>();
+
+            Item atkBuff = new Item("Attack Buff", 5, 1, 10);
+            availableItems.Add(atkBuff);
+
+            Item defBuff = new Item("Defense Buff", 5, 1, 10);
+            availableItems.Add(atkBuff);
+
+            Item speedBuff = new Item("Speed Buff", 5, 1, 10);
+            availableItems.Add(speedBuff);
+
+            Item specAtkBuff = new Item("Special Attack Buff", 5, 1, 10);
+            availableItems.Add(specAtkBuff);
+
+            Item net = new Item("Net", 0, 1, 30);
+            availableItems.Add(net);
+        }
+
+        public void PurchaseItem(List<Item> playerList, Item shopItem, Player player1)
+        {
+            if (player1.Money >= shopItem.Price)
+            {
+                player1.Money = player1.Money - shopItem.Price;
+
+                playerList.Add(shopItem);
+            }
+            else
+            {
+                // Display some message?
+            }
+        }
+    }
+}
